9a103e364a0cf095a6279a188292334b96f3fff8
safouraodo <136112069+safouraodo@users.noreply.github.com>
2023-07-06 13:14:14 +0000

    divers
    test

---
+++ bienvenue.php
@@ -0,0 +1,21 @@
+
+<body>
+
+  
+
+<main id="main" class="main">
+<style>
+body {
+  background-image: url('../assets/img/fibre_optique.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+
+<h1 style="text-align:center; color:#FFFF00;"> BIENVENUE <?= $_SESSION['nom']." ".$_SESSION['prenoms']?> </h1>
+
+
+</p>
+
+</body>
